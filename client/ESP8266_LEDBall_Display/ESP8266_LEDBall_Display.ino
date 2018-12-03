aece639e6698b5ac1a4108de9bff505f57660308
Robin <kremerrobin@gmx.de>
2018-12-03 19:16:57 +0100

    Added Gamma Corretion

--- client/ESP8266_LEDBall_Display/ESP8266_LEDBall_Display.ino
+++ client/ESP8266_LEDBall_Display/ESP8266_LEDBall_Display.ino
@@ -47,7 +47,7 @@ int maxFPS = 60;
 int matrixUpdatePeriod = int(1.0 / maxFPS * 1000);
 //------------------  Debug  ------------------
 int loopspersec = 0;
-
+extern const uint8_t gamma8[];
 
 //------------------  Setup  ------------------
 void setup() {
@@ -74,7 +74,7 @@ void setup() {
   pinMode(statusled, OUTPUT); //Debug Status LED
 
   LEDS.addLeds<WS2812, MATRIXPIN, RGB>(leds, LEDCOUNT);
-  LEDS.setBrightness(40);
+  LEDS.setBrightness(255);
 }
 
 //------------------  Loop  ------------------
@@ -140,15 +140,15 @@ void simpleUpdateMatrix() {
     for (int x = 0; x < LEDMATRIXWIDTH; x++) { //Rows
       for (int y = 0; y < LEDMATRIXHEIGHT; y++) { //Column
         if (x % 2 == 0) { //even Rows
-          leds[x * LEDMATRIXHEIGHT + y][0] = ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3]; // RED
-          leds[x * LEDMATRIXHEIGHT + y][1] = ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3 + 1]; // GREEN
-          leds[x * LEDMATRIXHEIGHT + y][2] = ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3 + 2]; // BLUE
+          leds[x * LEDMATRIXHEIGHT + y][0] = gamma8[ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3]]; // RED
+          leds[x * LEDMATRIXHEIGHT + y][1] = gamma8[ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3 + 1]]; // GREEN
+          leds[x * LEDMATRIXHEIGHT + y][2] = gamma8[ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3 + 2]]; // BLUE
         }
         else {
           int reverseY = LEDMATRIXHEIGHT - y - 1;
-          leds[x * LEDMATRIXHEIGHT + reverseY][0] = ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3]; // RED
-          leds[x * LEDMATRIXHEIGHT + reverseY][1] = ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3 + 1]; // GREEN
-          leds[x * LEDMATRIXHEIGHT + reverseY][2] = ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3 + 2]; // BLUE
+          leds[x * LEDMATRIXHEIGHT + reverseY][0] = gamma8[ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3]]; // RED
+          leds[x * LEDMATRIXHEIGHT + reverseY][1] = gamma8[ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3 + 1]]; // GREEN
+          leds[x * LEDMATRIXHEIGHT + reverseY][2] = gamma8[ledFrameBuffer[x * 3 + LEDMATRIXWIDTH * y * 3 + 2]]; // BLUE
         }
       }
     }
@@ -235,4 +235,24 @@ void printLEDFrameBuffer() {
     Serial.println("");
   }
 }
+
+
+const uint8_t gamma8[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+  2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
+  5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10,
+  10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+  17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+  25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+  37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+  51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+  69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+  90, 92, 93, 95, 96, 98, 99, 101, 102, 104, 105, 107, 109, 110, 112, 114,
+  115, 117, 119, 120, 122, 124, 126, 127, 129, 131, 133, 135, 137, 138, 140, 142,
+  144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 167, 169, 171, 173, 175,
+  177, 180, 182, 184, 186, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213,
+  215, 218, 220, 223, 225, 228, 231, 233, 236, 239, 241, 244, 247, 249, 252, 255
+};
 
